5d66f038d6c82452fd16b9cb08ea5d1655aa5f9a
bhavaygoel <goelbhavay@gmail.com>
2023-04-27 18:04:54 +0530

    Styled and added automatic request to backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,50 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import "./index.css";
 
 const App: React.FC = () => {
   const [response, setResponse] = useState<string>("");
   const [url, setUrl] = useState<string>("");
-  const [detail, setDetail] = useState<boolean>(false);
-  const [points, setPoints] = useState<boolean>(false);
-
+  const [detail, setDetail] = useState<boolean>(true);
+  const [points, setPoints] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   useEffect(() => {
     const queryInfo = { active: true, lastFocusedWindow: true };
 
     chrome.tabs &&
       chrome.tabs.query(queryInfo, (tabs) => {
-        const url = tabs[0].url;
-        if (url) setUrl(url);
+        const siteUrl = tabs[0].url;
+        if (siteUrl) setUrl(siteUrl);
       });
   }, []);
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setResponse("");
-    try {
-      const res = await fetch("http://127.0.0.1:5000/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ url, detail, points }),
-      });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://127.0.0.1:5000/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ url, detail, points }),
+        });
 
-      if (!res.ok) {
-        throw new Error("Failed to fetch");
+        if (!res.ok) {
+          throw new Error("Failed to fetch");
+        }
+
+        const result = await res.json();
+        console.log(result);
+        setResponse(result.summary);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
       }
+    };
 
-      const result = await res.json();
-      console.log(result);
-      setResponse(result.summary);
-    } catch (error) {
-      console.error(error);
+    if (url) {
+      fetchData();
     }
-  };
+  }, [url, detail, points]);
 
   const handleUrlChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUrl(event.target.value);
@@ -51,39 +57,48 @@ const App: React.FC = () => {
   const handlePointsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPoints(event.target.checked);
   };
+  let circleCommonClasses = "h-3.5 w-3.5 bg-pink-500 rounded-full";
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="url">URL</label>
-        <input
-          type="text"
-          name="url"
-          value={url}
-          onChange={handleUrlChange}
-          required
-        />
-        <br />
-        <label htmlFor="detail">Summarize in detail?</label>
-        <input
-          type="checkbox"
-          name="detail"
-          checked={detail}
-          onChange={handleDetailChange}
-        />
-        <br />
-        <label htmlFor="points">in points?</label>
-        <input
-          type="checkbox"
-          name="points"
-          checked={points}
-          onChange={handlePointsChange}
-        />
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-
-      <div className="display-linebreak">{response && <p>{response}</p>}</div>
+    <div className="px-5 py-5 ">
+      {isLoading ? (
+        <div className="flex justify-center items-center h-96 w-full">
+          <div className={`${circleCommonClasses} mr-1 animate-bounce`}></div>
+          <div
+            className={`${circleCommonClasses} mr-1 animate-bounce200`}
+          ></div>
+          <div className={`${circleCommonClasses} animate-bounce400`}></div>
+        </div>
+      ) : (
+        <>
+          <div className="rounded-lg bg-slate-200 p-5">
+            {response && <p className="whitespace-pre-wrap">{response}</p>}
+          </div>
+          <h1 className="mt-5 font-semibold ">Summary options:</h1>
+          <form className="flex flex-row justify-start" action="">
+            <div>
+              <label htmlFor="detail">Detailed</label>
+              <input
+                type="checkbox"
+                name="detail"
+                checked={detail}
+                className="ml-1 relative bottom-0.5 rounded text-pink-500"
+                onChange={handleDetailChange}
+              />
+            </div>
+            <div className="ml-4">
+              <label htmlFor="points">Points</label>
+              <input
+                type="checkbox"
+                name="points"
+                checked={points}
+                className="ml-1 relative bottom-0.5 rounded text-pink-500"
+                onChange={handlePointsChange}
+              />
+            </div>
+          </form>
+        </>
+      )}
     </div>
   );
 };
--- server/edgegpt.py
+++ server/edgegpt.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 CORS(app)
 bot = Chatbot(cookie_path='./cookies.json')
 
+
 @app.route('/', methods=['POST'])
 async def chatbot():
     data = request.json
@@ -15,6 +16,12 @@ async def chatbot():
     detail = data.get('detail', False)
     points = data.get('points', False)
     url = data.get('url', '')
+    print(url + "is the url")
+    if url == '':
+        return jsonify({
+            "summary": "Please open it in a tab with url",
+            "isLoading": "false"
+        })
     prompt = url + " summarize the content on the webpage"
     if detail:
         prompt += " in detail"
@@ -25,9 +32,15 @@ async def chatbot():
     for message in response['item']['messages']:
         if message['author'] == 'bot':
             text = message['text']
-            text = re.sub(r'\[\^1\^\]', '', text)    # add "-" to beginning of each line
+            # add "-" to beginning of each line
+            text = re.sub(r'\[\^[^\]]+\^\]', '', text)
+            text = text.replace(" -", "\n-")
             result.append(text)
-    return jsonify({"summary": result})
+    print("sending the result")
+    return jsonify({
+        "summary": result,
+        "isLoading": "false"
+        })
 
 
 if __name__ == '__main__':
